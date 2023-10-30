1627c0244e1573e7a1b4f85faa3873665bc2c3c5
CinegencePy <cinegence.python@gmail.com>
2023-10-30 14:52:13 +0530

    first commit

--- backend/visitor_project/settings.py
+++ backend/visitor_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -51,7 +52,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-    
+
 ]
 
 ROOT_URLCONF = 'visitor_project.urls'
